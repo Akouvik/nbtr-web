62ad22391e1a4763fdf883d3e06f6523fb454719
EC2 Default User <ec2-user@ip-172-31-51-207.ec2.internal>
2021-07-06 17:54:28 +0000

    checkboxes implemented for slides

--- src/routes/Slides/SlideFormsandInputs.js
+++ src/routes/Slides/SlideFormsandInputs.js
@@ -6,58 +6,58 @@ const SlideFormsandInputs = () =>{
         e.preventDefault()
     }
     return (
-        <form className="px-5 py-3 slideform" onSubmit={handleSubmit} >
-            <div className="col-md-7 mx-auto">
+        <form className="slideform" onSubmit={handleSubmit} >
+            <div className="mx-auto">
              <div>
-                <label> Subnum: </label>  <br/>
+                <h3> Subnum: </h3>  <br/>
                 <input type="text" placeholder="Subnum" name="subnum" />
              </div>
+             
             <div>
-                <label> Min Age:</label>  <br/>
-                <input type="number" placeholder="Age" />
-            </div>
-            <div>
-            <label> Max Age:</label>  <br/>
-                <input type="number" placeholder="Max Age" />
+                <h3> Age </h3>  <br/>
+                <input type="number" placeholder="Min Age" /> <input type="number" placeholder="Max Age" />
             </div>
 
             <div>
-                <label> Sex: </label>  <br/>
+                <h3> Sex </h3>  <br/>
                     <input type="checkbox" /> Male 
                     <input type="checkbox" /> Female 
                     <input type="checkbox" /> Both 
             </div>
             
             <div>
-                <label>Race:</label>  <br/>
-                    <input type="checkbox" />Asian
-                    <input type="checkbox" />Black
-                    <input type="checkbox" />Hispanic
-                    <input type="checkbox" />White
-                    <input type="checkbox" />Other
-                    <input type="checkbox" />Unknown
+                <h3>Race</h3>  <br/>
+                   Asian <input type="checkbox" />
+                   Black <input type="checkbox" />
+                    Hispanic <input type="checkbox" />
+                    White <input type="checkbox" />
+                    Other <input type="checkbox" />
+                    Unknown <input type="checkbox" />
             </div>
+            
             <div>        
-                <label>Brain Region:</label> <br/>
+                <h3>Brain Region</h3> <br/>
                     Amygdala <input type="checkbox" />
-                    <input type="checkbox" />Basal Ganglia
-                    <input type="checkbox" />Basal Nucleus of Myenert
-                    <input type="checkbox" />Calcarine Cortex
-                    <input type="checkbox" />Caudal Hippocampus
-                    <input type="checkbox" />Cerebellum
-                    <input type="checkbox" />Cingulate Gyrus
-                    <input type="checkbox" />Inferior Parietal Lobule
-                    <input type="checkbox" />Medulla
-                    <input type="checkbox" />Midbrain
-                    <input type="checkbox" />Middle Frontal Gyrus
-                    <input type="checkbox" />Orbital Frontal Cortex
-                    <input type="checkbox" />Pons
-                    <input type="checkbox" />Rostral Hippocampus
-                    <input type="checkbox" />Superior Temporal
-                    <input type="checkbox" />Thalamus and Basal Ganglia
+                   Basal Ganglia <input type="checkbox" />
+                    Basal Nucleus of Myenert <input type="checkbox" />
+                    Calcarine Cortex <input type="checkbox" />
+                    Caudal Hippocampus <input type="checkbox" />
+                    Cerebellum <input type="checkbox" />
+                    Cingulate Gyrus <input type="checkbox" />
+                    Inferior Parietal Lobule <input type="checkbox" />
+                    Medulla <input type="checkbox" />
+                    Midbrain <input type="checkbox" />
+                    Middle Frontal Gyrus <input type="checkbox" />
+                    Orbital Frontal Cortex <input type="checkbox" />
+                    Pons <input type="checkbox" />
+                    Rostral Hippocampus <input type="checkbox" />
+                    Superior Temporal <input type="checkbox" />
+                    Thalamus and Basal Ganglia <input type="checkbox" />
             </div>
+            
+            
             <div>
-                <label>Stain:</label>  <br/>
+                <h3>Stain</h3>  <br/>
                     Amyloid Beta <input type="checkbox" />
                     Congo Red <input type="checkbox" />
                     GFAP <input type="checkbox" />
@@ -72,10 +72,10 @@ const SlideFormsandInputs = () =>{
                     SV40-IHC <input type="checkbox" />
                     Synuclein <input type="checkbox" />
                     Tau <input type="checkbox" />
-                    <input type="checkbox" />TDP-43
-                    <input type="checkbox" />TG3
-                    <input type="checkbox" />Thioflavin S
-                    <input type="checkbox" />Ubiquitin
+                    TDP-43<input type="checkbox" />
+                    TG3<input type="checkbox" />
+                    Thioflavin S <input type="checkbox" />
+                    Ubiquitin <input type="checkbox" />
             </div>
             
             <input type="Submit" class="btn btn-primary submitbtn" id="submitQueryinput"/>
