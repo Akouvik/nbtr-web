dbc3c418588bbfca207985c2f141fb30162563db
EC2 Default User <ec2-user@ip-172-31-51-207.ec2.internal>
2021-07-06 18:49:26 +0000

    checkboxes implemented for slides

--- src/routes/Slides/SlideFormsandInputs.js
+++ src/routes/Slides/SlideFormsandInputs.js
@@ -6,57 +6,61 @@ const SlideFormsandInputs = () =>{
         e.preventDefault()
     }
     return (
-        <div className="row">
+        <div>
             <form className="slideform" onSubmit={handleSubmit} >
-                <div className="mx-auto">
-                 <div>
-                    <h4> Subnum: </h4>
-                    <input type="text" placeholder="Subnum" name="subnum" />
-                 </div>
-                 
-                <div>
-                    <h4> Age </h4>
-                    <input type="number" placeholder="Min Age" /> <input type="number" placeholder="Max Age" />
-                </div>
-    
-                <div>
-                    <h4> Sex </h4>
-                    <ul>
-                        <li> <input type="checkbox" /> Male </li>
-                        <li> <input type="checkbox" /> Female </li>
-                        <li> <input type="checkbox" /> Both </li>
-                    </ul>
+                <div  className="row">
+                     <div className="col">
+                        <h4> Subnum: </h4>
+                        <input type="text" placeholder="Subnum" name="subnum" />
+                     </div>
+                     
+                    <div className="col">
+                        <h4> Age </h4>
+                        <input type="number" placeholder="Min Age" /> <input type="number" placeholder="Max Age" />
+                    </div>
                 </div>
                 
-                <div>
-                    <h4>Race</h4>
-                    <ul>
-                        <li>Asian <input type="checkbox" /></li>
-                       <li> Black <input type="checkbox" /></li>
-                        <li> Hispanic <input type="checkbox" /></li>
-                         <li>White <input type="checkbox" /></li>
-                        <li> Other <input type="checkbox" /></li>
-                        <li> Unknown <input type="checkbox" /></li>
-                    </ul>
-                </div>
+                <div className="row">
                 
-                <div> 
-                
-                    <h4>Brain Region</h4>
-                    <div className="col-lg-6 ">
-                    <ul>
-                        <li> Amygdala <input type="checkbox" /> </li>
-                        <li> Basal Ganglia <input type="checkbox" /> </li>
-                        <li>  Basal Nucleus of Myenert <input type="checkbox" /></li>
-                        <li>  Calcarine Cortex <input type="checkbox" /></li>
-                        <li>  Caudal Hippocampus <input type="checkbox" /></li>
-                        <li>  Cerebellum <input type="checkbox" /></li>
-                        <li>  Cingulate Gyrus <input type="checkbox" /></li>
-                        <li>  Inferior Parietal Lobule <input type="checkbox" /></li>
-                    </ul>
+                    <div className="col">
+                        <h4> Sex </h4>
+                         <ul>
+                            <li>Male <input type="checkbox" />  </li>
+                            <li>Female <input type="checkbox" />  </li>
+                            <li>Both <input type="checkbox" />  </li>
+                        </ul>
                     </div>
                     
-                    <div className="col-lg-6">
+                    <div className="col">
+                        <h4>Race</h4>
+                        <ul>
+                            <li>Asian <input type="checkbox" /></li>
+                           <li> Black <input type="checkbox" /></li>
+                            <li> Hispanic <input type="checkbox" /></li>
+                             <li>White <input type="checkbox" /></li>
+                            <li> Other <input type="checkbox" /></li>
+                            <li> Unknown <input type="checkbox" /></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div className="row"> 
+                
+                    <h4>Brain Region</h4>
+                    <div className="col">
+                        <ul>
+                            <li> Amygdala <input type="checkbox" /> </li>
+                            <li> Basal Ganglia <input type="checkbox" /> </li>
+                            <li>  Basal Nucleus of Myenert <input type="checkbox" /></li>
+                            <li>  Calcarine Cortex <input type="checkbox" /></li>
+                            <li>  Caudal Hippocampus <input type="checkbox" /></li>
+                            <li>  Cerebellum <input type="checkbox" /></li>
+                            <li>  Cingulate Gyrus <input type="checkbox" /></li>
+                            <li>  Inferior Parietal Lobule <input type="checkbox" /></li>
+                        </ul>
+                    </div>
+                    
+                    <div className="col">
                         <ul>
                             <li> Medulla <input type="checkbox" /></li>
                             <li> Midbrain <input type="checkbox" /></li>
@@ -72,9 +76,9 @@ const SlideFormsandInputs = () =>{
                 </div>
                 
                 
-                <div>
+                <div className="row">
                     <h4>Stain</h4>
-                    <div className="col-lg-6">
+                    <div className="col">
                         <ul>
                             <li> Amyloid Beta <input type="checkbox" /></li>
                             <li> Congo Red <input type="checkbox" /></li>
@@ -88,9 +92,8 @@ const SlideFormsandInputs = () =>{
                          </ul>
                     </div>
                     
-                    <div className="col-lg-6">
+                    <div className="col">
                         <ul>
-                            <li> Other <input type="checkbox" /></li>
                             <li>Perls Fe <input type="checkbox" /></li>
                             <li>  SV40-IHC <input type="checkbox" /></li>
                             <li> Synuclein <input type="checkbox" /></li>
@@ -99,14 +102,12 @@ const SlideFormsandInputs = () =>{
                             <li>TG3<input type="checkbox" /></li>
                             <li>Thioflavin S <input type="checkbox" /></li>
                             <li> Ubiquitin <input type="checkbox" /></li>
+                            <li> Other <input type="checkbox" /></li>
                         </ul>
                     </div>
-                    
-                    
                 </div>
                 
                 <input type="Submit" class="btn btn-primary submitbtn" id="submitQueryinput"/>
-                </div>
             </form>
     </div>
         )
