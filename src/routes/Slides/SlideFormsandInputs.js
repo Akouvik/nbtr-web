67de7e7c7acf40bb5ac735e5af518047eabb488d
EC2 Default User <ec2-user@ip-172-31-51-207.ec2.internal>
2021-07-07 14:24:44 +0000

    slides checkbox update

--- src/routes/Slides/SlideFormsandInputs.js
+++ src/routes/Slides/SlideFormsandInputs.js
@@ -3,7 +3,8 @@ import useForm from 'react-hook-form';
 // import  Sex  from "../../utils/Sex"
 
 
-const  Sex = ["Male","Female","Both"]
+const  Sex = ["Male","Female","Both"];
+const  Race = ["Asian","Black","Hispanic","White", "Other","Unknown"];
 
 
 const SlideFormsandInputs = () =>{
@@ -30,23 +31,38 @@ const SlideFormsandInputs = () =>{
                      
                     <div className="col">
                         <h4> Age </h4>
-                        <input type="number" placeholder="Min Age" /> :<input type="number" placeholder="Max Age" />
+                        <input type="number" placeholder="Min Age" /><input type="number" placeholder="Max Age" />
                     </div>
                 </div>
                 
                 <div className="row">
                 
                     <div className="col">
-                        <h4> Sex </h4>
+                        <h5> Sex </h5>
                          <ul className="sex">
                             {Sex.map((sex,index) => {
                                 return (
                                 <li key={index}>
-\                                    <label htmlFor={`custom-checkbox-${index}`}>{sex}</label><input type="checkbox" id={`custom-checkbox-${index}`} name={sex} value={sex}/>
+                                   <label htmlFor={`custom-checkbox-${index}`}>{sex}</label><input type="checkbox" id={`custom-checkbox-${index}`} name={sex} value={sex}/>
                                 </li>
                                 )
                             })}
                         </ul>
+                    </div>
+                    
+                    <div className="col">
+                        <div>
+                            <h5>Race</h5>
+                            <ul className="race">
+                            {Race.map((race,index) =>{
+                                return(
+                                <li key={index}>
+                                <label htmlFor={`custom-checkbox-${index}`}>{race}</label><input type="checkbox" id={`custom-checkbox-${index}`} name={race} value={race} />
+                                </li>
+                                )
+                            })}
+                            </ul>
+                        </div>
                     </div>
                 </div>   
             </form>
